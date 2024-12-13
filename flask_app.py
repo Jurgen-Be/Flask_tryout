886b182a77d0985843f8cd67b821a3db694b6c6e
Jurgen B <jurgenamanda@gmail.com>
2024-12-13 09:24:23 +0100

    basic Flask code

---
+++ flask_app.py
@@ -0,0 +1,12 @@
+# Import modules
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<p>Hello World!<p>"
+
+# Hoofdprogramma
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
